5effea67d90ab4dedb7c726ecfe2b17c5ec89c66
vivek naik <kevivforever>
2016-11-17 18:14:26 +0530

    click event added on infowindow click

--- app/src/main/java/com/waiapp/WaiApplication.java
+++ app/src/main/java/com/waiapp/WaiApplication.java
@@ -15,7 +15,8 @@ public class WaiApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder(this)
+        Realm.init(this);
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
                 .name(Realm.DEFAULT_REALM_NAME)
                 .schemaVersion(0)
                 .deleteRealmIfMigrationNeeded()
