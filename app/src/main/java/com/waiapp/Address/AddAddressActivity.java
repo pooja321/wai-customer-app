f20327ac463086939babb061ce7654eb218de34d
WINDOW64BIT-PC\WINDOW 64 BIT <swati562.sd@gmail.com>
2016-11-18 14:58:41 +0530

    Validation for Addaddressactivity and editaddressactivity

--- app/src/main/java/com/waiapp/Address/AddAddressActivity.java
+++ app/src/main/java/com/waiapp/Address/AddAddressActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -35,8 +36,12 @@ public class AddAddressActivity extends AppCompatActivity implements View.OnClic
     private ProgressDialog mSaveProgressDialog;
 
     String addressName, addressid, addressType, houseNo, areaName, landmark, city, state, country, pincode, UID;
-    public static final String selectAddressTypeLabel = "Select Address Type";
+    public static final String selectAddressTypeLabel = "Select Address Type*";
     private String[] addressTypeList = new String[]{selectAddressTypeLabel, "Flat", "House"};
+
+    boolean failFlag = false;
+
+    TextInputLayout mTIL_Address_title, mTIL_Housenum, mTIL_Area_name, mTIL_Landmark, mTIL_City, mTIL_State, mTIL_Pincode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +63,15 @@ public class AddAddressActivity extends AppCompatActivity implements View.OnClic
         mEditTextCity = (EditText) findViewById(R.id.addaddress_et_city);
         mEditTextPincode = (EditText) findViewById(R.id.addaddress_et_pincode);
         mEditTextState = (EditText) findViewById(R.id.addaddress_et_state);
+
+
+        mTIL_Address_title = (TextInputLayout) findViewById(R.id.til_address_title);
+        mTIL_Housenum = (TextInputLayout) findViewById(R.id.til_house_no);
+        mTIL_Area_name = (TextInputLayout) findViewById(R.id.til_area_name);
+        mTIL_Landmark = (TextInputLayout) findViewById(R.id.til_landmark);
+        mTIL_City = (TextInputLayout) findViewById(R.id.til_city);
+        mTIL_State = (TextInputLayout) findViewById(R.id.til_State);
+        mTIL_Pincode = (TextInputLayout) findViewById(R.id.til_pincode);
 
         mSpinnerAddressType = (Spinner) findViewById(R.id.addaddress_spinner_address_type);
         if (mSpinnerAddressType != null) {
@@ -81,7 +95,6 @@ public class AddAddressActivity extends AppCompatActivity implements View.OnClic
         int id = v.getId();
         switch (id) {
             case (R.id.addaddress_button_submit):
-                mSaveProgressDialog.show();
                 addAddress(v);
                 break;
         }
@@ -97,27 +110,95 @@ public class AddAddressActivity extends AppCompatActivity implements View.OnClic
         state = mEditTextState.getText().toString();
         country = "India";
 
+        String EmptyString = getResources().getString(R.string.provide_necessary_details);
+        String Provide_10_characters = getResources().getString(R.string.less_than_10_character);
+        String Provide_6_characters = getResources().getString(R.string.less_than_6_character);
+
+
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
         }
         addressid = mDatabase.child(Constants.FIREBASE_CHILD_ADDRESS).push().getKey();
 
+        if (addressName.equals("")) {
+            failFlag = true;
+            mTIL_Address_title.setError(EmptyString);
+        } else {
+            mTIL_Address_title.setErrorEnabled(false);
+        }
 
-        Address address = new Address(addressid, addressName, addressType, houseNo, areaName, landmark, city, state, country, pincode);
+        if (houseNo.equals("")) {
+            failFlag = true;
+            mTIL_Housenum.setError(EmptyString);
+        } else {
+            mTIL_Housenum.setErrorEnabled(false);
+        }
+        if (areaName.equals("")) {
+            failFlag = true;
+            mTIL_Area_name.setError(EmptyString);
+        } else {
+            mTIL_Area_name.setErrorEnabled(false);
+        }
+        if (landmark.equals("")) {
+            failFlag = true;
+            mTIL_Landmark.setError(EmptyString);
+        } else {
+            mTIL_Landmark.setErrorEnabled(false);
+        }
+        if (city.equals("")) {
+            failFlag = true;
+            mTIL_City.setError(EmptyString);
+        } else {
+            mTIL_City.setErrorEnabled(false);
+        }
+        if (state.equals("")) {
+            failFlag = true;
+            mTIL_State.setError(EmptyString);
+        } else {
+            mTIL_State.setErrorEnabled(false);
+        }
+        if (pincode.equals("")) {
+            failFlag = true;
+            mTIL_Pincode.setError(EmptyString);
+        } else {
+            mTIL_Pincode.setErrorEnabled(false);
+        }
+        if (houseNo.length() < 10 && houseNo.length() > 0) {
+            failFlag = true;
+            mTIL_Housenum.setError(Provide_10_characters);
+        } else {
+            mTIL_Housenum.setErrorEnabled(false);
+        }
+        if (pincode.length() < 6 && houseNo.length() > 0) {
+            failFlag = true;
+            mTIL_Pincode.setError(Provide_6_characters);
+        } else {
+            mTIL_Pincode.setErrorEnabled(false);
+        }
+        if (addressType==selectAddressTypeLabel)
+        {
+            Toast.makeText(this, "Please select valid Address Type", Toast.LENGTH_SHORT).show();
+        }
 
-        mDatabase.child(Constants.FIREBASE_CHILD_ADDRESS).child(UID).child(addressid).setValue(address).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if (!task.isSuccessful()) {
-                    Toast.makeText(AddAddressActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(AddAddressActivity.this, "Addess Added", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(AddAddressActivity.this, AddressActivity.class));
+        if (failFlag == false) {
+            mSaveProgressDialog.show();
+
+            Address address = new Address(addressid, addressName, addressType, houseNo, areaName, landmark, city, state, country, pincode);
+
+            mDatabase.child(Constants.FIREBASE_CHILD_ADDRESS).child(UID).child(addressid).setValue(address).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(AddAddressActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(AddAddressActivity.this, "Addess Added", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(AddAddressActivity.this, AddressActivity.class));
+                    }
+                    mSaveProgressDialog.dismiss();
                 }
-                mSaveProgressDialog.dismiss();
-            }
-        });
+            });
+        }
     }
 
     @Override
--- app/src/main/java/com/waiapp/Address/EditAddressActivity.java
+++ app/src/main/java/com/waiapp/Address/EditAddressActivity.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -21,11 +24,17 @@ import com.waiapp.Utility.Constants;
 
 public class EditAddressActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
+    private Spinner mSpinnerAddressType;
 
     private EditText mEditTextAddressName, mEditTextHouseNo, mEditTextAreaName, mEditTextLandMark, mEditTextCity, mEditTextPincode, mEditTextState;
     private Button mButtonSubmit;
     Address mAddress = new Address();
-    String addressName, addressid, addressType, houseNo, areaName, landmark, city, state, country, pincode, UID;
+    String addressName, addressType, houseNo, areaName, landmark, city, state, country, pincode, UID;
+
+    boolean failFlag = false;
+    public static final String selectAddressTypeLabel = "Select Address Type*";
+    private String[] addressTypeList = new String[]{selectAddressTypeLabel, "Flat", "House"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,14 +43,14 @@ public class EditAddressActivity extends AppCompatActivity {
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        mEditTextAddressName = (EditText) findViewById(R.id.addaddress_et_address_name);
-        mEditTextHouseNo = (EditText) findViewById(R.id.addaddress_et_houseno);
-        mEditTextAreaName = (EditText) findViewById(R.id.addaddress_et_areaname);
-        mEditTextLandMark = (EditText) findViewById(R.id.addaddress_et_landmark);
-        mEditTextCity = (EditText) findViewById(R.id.addaddress_et_city);
-        mEditTextPincode = (EditText) findViewById(R.id.addaddress_et_pincode);
-        mEditTextState = (EditText) findViewById(R.id.addaddress_et_state);
-        mButtonSubmit = (Button) findViewById(R.id.address_button_submit);
+        mEditTextAddressName = (EditText) findViewById(R.id.editaddress_et_address_name);
+        mEditTextHouseNo = (EditText) findViewById(R.id.editaddress_et_houseno);
+        mEditTextAreaName = (EditText) findViewById(R.id.editaddress_et_areaname);
+        mEditTextLandMark = (EditText) findViewById(R.id.editaddress_et_landmark);
+        mEditTextCity = (EditText) findViewById(R.id.editaddress_et_city);
+        mEditTextPincode = (EditText) findViewById(R.id.editaddress_et_pincode);
+        mEditTextState = (EditText) findViewById(R.id.editaddress_et_state);
+        mButtonSubmit = (Button) findViewById(R.id.editaddress_button_submit);
 
         mAddress = (Address) getIntent().getSerializableExtra("Address");
 
@@ -53,6 +62,28 @@ public class EditAddressActivity extends AppCompatActivity {
         mEditTextPincode.setText(mAddress.getPincode());
         mEditTextState.setText(mAddress.getState());
 
+
+        mSpinnerAddressType = (Spinner) findViewById(R.id.editaddress_spinner_address_type);
+        if (mSpinnerAddressType != null) {
+            mSpinnerAddressType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    Spinner addressTypeSpinner = (Spinner) parent;
+                    if (addressTypeSpinner.getId() == R.id.editaddress_spinner_address_type) {
+                        addressType = parent.getItemAtPosition(position).toString();
+                    }
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+        }
+
+        ArrayAdapter<String> genderAdapter = new ArrayAdapter<String>(EditAddressActivity.this, android.R.layout.simple_spinner_dropdown_item, addressTypeList);
+        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinnerAddressType.setAdapter(genderAdapter);
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
@@ -70,21 +101,68 @@ public class EditAddressActivity extends AppCompatActivity {
                 pincode = mEditTextPincode.getText().toString();
                 state = mEditTextState.getText().toString();
                 country = "India";
-                Address address = new Address(mAddress.getAddressId(), addressName, addressType, houseNo, areaName, landmark, city, state, country, pincode);
-                mDatabase.child(Constants.FIREBASE_CHILD_ADDRESS).child(UID).child(address.getAddressId()).setValue(address).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (!task.isSuccessful()) {
-                            Toast.makeText(EditAddressActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(EditAddressActivity.this, "Addess Added", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(EditAddressActivity.this, AddressActivity.class));
+
+                String EmptyString = getResources().getString(R.string.provide_necessary_details);
+                String Provide_10_characters = getResources().getString(R.string.less_than_10_character);
+                String Provide_6_characters = getResources().getString(R.string.less_than_6_character);
+
+
+                if (addressName.equals("")) {
+                    failFlag = true;
+                    mEditTextAddressName.setError(EmptyString);
+                }
+                if (houseNo.equals("")) {
+                    failFlag = true;
+                    mEditTextHouseNo.setError(EmptyString);
+                }
+                if (areaName.equals("")) {
+                    failFlag = true;
+                    mEditTextAreaName.setError(EmptyString);
+                }
+                if (landmark.equals("")) {
+                    failFlag = true;
+                    mEditTextLandMark.setError(EmptyString);
+                }
+                if (city.equals("")) {
+                    failFlag = true;
+                    mEditTextCity.setError(EmptyString);
+                }
+                if (state.equals("")) {
+                    failFlag = true;
+                    mEditTextState.setError(EmptyString);
+                }
+                if (pincode.equals("")) {
+                    failFlag = true;
+                    mEditTextPincode.setError(EmptyString);
+                }
+                if (houseNo.length() < 10 && houseNo.length() > 0) {
+                    failFlag = true;
+                    mEditTextHouseNo.setError(Provide_10_characters);
+                }
+                if (pincode.length() < 6 && houseNo.length() > 0) {
+                    failFlag = true;
+                    mEditTextPincode.setError(Provide_6_characters);
+                }
+                if (addressType == selectAddressTypeLabel) {
+                    Toast.makeText(EditAddressActivity.this, "Please select valid Address Type", Toast.LENGTH_SHORT).show();
+                } else if (failFlag == false) {
+                    Address address = new Address(mAddress.getAddressId(), addressName, addressType, houseNo, areaName, landmark, city, state, country, pincode);
+
+                    mDatabase.child(Constants.FIREBASE_CHILD_ADDRESS).child(UID).child(address.getAddressId()).setValue(address).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(EditAddressActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(EditAddressActivity.this, "Addess Added", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(EditAddressActivity.this, AddressActivity.class));
+                            }
                         }
-                    }
-                });
+                    });
+                }
             }
+
         });
-
-
     }
 }
+
